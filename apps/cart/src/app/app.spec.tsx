d66cb7fae30a517190f96c2b15e56e7b0cf19e2f
Gabriel Luiz <gabrielluizs1996@gmail.com>
2026-01-22 14:26:59 -0300

    feat: startup config module federation NX

--- apps/cart/src/app/app.spec.tsx
+++ apps/cart/src/app/app.spec.tsx
@@ -11,7 +11,7 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getAllByText } = render(<App />);
     expect(
-      getAllByText(new RegExp('Welcome shop', 'gi')).length > 0
+      getAllByText(new RegExp('Welcome cart', 'gi')).length > 0
     ).toBeTruthy();
   });
 });
---
+++ apps/shell/src/app/nx-welcome.tsx
@@ -0,0 +1,17 @@
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ This is a starter component and can be deleted.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Delete this file and get started with your project!
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+export function NxWelcome({ title }: { title: string }) {
+  return (
+    <h1>
+      <span> Hello there, </span>
+      Welcome {title} 👋
+    </h1>
+  );
+}
+
+export default NxWelcome;
