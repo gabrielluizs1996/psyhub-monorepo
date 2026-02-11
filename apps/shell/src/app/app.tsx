b514e20a715f7e02a1a25812d8caa33a46124602
Gabriel Luiz <gabrielluizs1996@gmail.com>
2026-02-10 22:07:54 -0300

    feat: implements auth page

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -3,6 +3,7 @@ import NxWelcome from './nx-welcome';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { Header } from '../components';
 
+const Auth = React.lazy(() => import('auth/Module'));
 const Shop = React.lazy(() => import('shop/Module'));
 const Cart = React.lazy(() => import('cart/Module'));
 
@@ -12,10 +13,11 @@ export function App() {
   return (
     <React.Suspense fallback={null}>
       <div className="min-h-screen bg-background">
-        <Header mode={null} onLogoClick={()  => navigate('/')} />
+        <Header mode={null} onLogoClick={() => navigate('/')} />
 
         <Routes>
           <Route path="/" element={<NxWelcome title="shell" />} />
+          <Route path="/login" element={<Auth />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
